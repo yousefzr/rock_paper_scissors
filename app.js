0cde67844749e23bb19025536c62dd68e13bcfea
Yousef <yroowalar@gmail.com>
2020-10-02 08:31:43 +0530

    Added the computerPlay function

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function computerPlay() {   
     let random = Math.floor((Math.random() * 3) + 1);
-    return random;
+
+    switch (random) {
+        case 1:
+            console.log('rock');
+            break;
+        case 2:
+            console.log('paper');
+            break;
+        case 3:
+            console.log('scissor');
+            break;
+    }
 }
 
 
