099090c2773f2cd1e1382c585181c502a1200ec7
Yousef <yroowalar@gmail.com>
2020-10-03 07:53:27 +0530

    Completed the playerRound function

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
 function computerPlay() {   
     let random = Math.floor((Math.random() * 3) + 1);
 
     switch (random) {
         case 1:
-            console.log('rock');
+            return 'rock';
             break;
         case 2:
-            console.log('paper');
+            return 'paper';
             break;
         case 3:
-            console.log('scissors');
+            return 'scissors';
             break;
     }
 }
 
 function playRound(playerSelection, computerSelection) {
+    let roundWinner;
     computerSelection = computerPlay();
-    let winner;
+
     playerSelection = prompt('What do you choose. Rock, Paper or Scissors?');
     playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection === 'rock') {
-
+    
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        roundWinner = 'You lose! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        roundWinner =  'You lose! Scissors beats Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        roundWinner = 'You lose! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        roundWinner = 'You win! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        roundWinner =  'You win! Scissors beats Paper';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        roundWinner = 'You win! Rock beats Scissors';
+    } else if (playerSelection === computerSelection) {
+        roundWinner = 'It\'s a tie';
+    } else {
+        roundWinner = 'something went wrong. Try again please.';
     }
+    return roundWinner;
 }
+
 
