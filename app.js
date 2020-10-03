ac90f4bb9a763052efc6aeb29e6bf4c89f40a111
Yousef <yroowalar@gmail.com>
2020-10-03 08:19:54 +0530

    Complated the main structure of the game. Yet to add the UI

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 let draws = 0;
 
-function computerPlay() {   
+function computerPlay () {   
     let random = Math.floor((Math.random() * 3) + 1);
 
     switch (random) {
@@ -18,7 +18,7 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) {
     let roundWinner;
     computerSelection = computerPlay();
 
@@ -27,22 +27,46 @@ function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === 'rock' && computerSelection === 'paper') {
         roundWinner = 'You lose! Paper beats Rock';
+        computerScore++;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         roundWinner =  'You lose! Scissors beats Paper';
+        computerScore++;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         roundWinner = 'You lose! Rock beats Scissors';
+        computerScore++;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         roundWinner = 'You win! Paper beats Rock';
+        playerScore++;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         roundWinner =  'You win! Scissors beats Paper';
+        playerScore++;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         roundWinner = 'You win! Rock beats Scissors';
+        playerScore++;
     } else if (playerSelection === computerSelection) {
         roundWinner = 'It\'s a tie';
+        draws++;
     } else {
         roundWinner = 'something went wrong. Try again please.';
     }
     return roundWinner;
 }
 
+function game () {
+    let gameWinner;
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
 
+    if (playerScore > computerScore) {
+        gameWinner = 'You Won!';
+    } else if (playerScore < computerScore) {
+        gameWinner = 'Sorry, You Lose, Computer Wins';
+    } else {
+        gameWinner = 'Yay! You both won';
+    }
+
+    return gameWinner;
+}
+
+game();
